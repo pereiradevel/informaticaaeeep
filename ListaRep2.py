b1577da637ef57afe4d1d16ba96bd9eb9f924e3d
pereiradevel <icaro.santana@aluno.ce.gov.br>
2025-08-12 10:22:03 -0300

    Nova Atividade

---
+++ ListaRep2.py
@@ -0,0 +1,60 @@
+#Lista 2
+#Questao 1
+#nota1 = float(input("Digite sua primeira nota:"))
+#nota2 = float(input("Digite sua segunda nota:"))
+#nota3 = float(input("Digite sua terceira nota:"))
+#nota = nota1 + nota2 + nota3
+#if (nota / 3) >= 6:
+    #print("Aprovado! Parabéns.")
+#else:
+    #print("Reprovado! Estude Mais.")
+
+
+#Questao 2
+#palavra = str(input(("Digite uma palavra:")))
+#contar = 0
+#for i in palavra:
+    #if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+        #contar+=1
+#print(f"A Palavra tem {contar} vogais.")
+
+#Questao 3 
+#num = int(input("Digite um número #inteiro:"))
+#if num %2:
+    #print("Número é impar!")
+#else:
+#    print("Número par!")
+
+#Questao 4
+#primeiro = input("Digite um  1 número:")
+#segundo = input("Digite um  2 número:")
+#terceiro = input("Digite um  3 número:")
+#quarto = input("Digite um  4 número:")
+#quinto = input("Digite um  5 número:")
+
+#maior = primeiro
+
+#if (segundo > maior):
+        #maior = segundo
+#if (terceiro > maior):
+        #maior = terceiro
+
+#if (quarto > maior):
+    #maior = quarto
+#if (quinto > maior):
+    #maior = quinto
+#print(f"Maior é {maior}")
+
+#menor = primeiro
+#if (segundo < maior):
+        #maior = segundo
+#if (terceiro < maior):
+        #maior = terceiro
+
+#if (quarto < maior):
+    #maior = quarto
+#if (quinto < maior):
+    #maior = quinto
+#print(f"Menor é {menor}")
+
+#Questao 5
